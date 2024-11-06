d2242bde075d74f916adc3b1b816a0064e1c0f73
vic <kozv89148@gmail.com>
2024-11-06 11:56:29 +0700

    start working on gpu

--- my_scripts/connect_mongo.py
+++ my_scripts/connect_mongo.py
@@ -1,7 +1,11 @@
 from pymongo import MongoClient
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 MONGO_URL = os.getenv('MONGO_URL')
+
+print('[MONGO_URL]', MONGO_URL)
 
 client = MongoClient(MONGO_URL)
 
